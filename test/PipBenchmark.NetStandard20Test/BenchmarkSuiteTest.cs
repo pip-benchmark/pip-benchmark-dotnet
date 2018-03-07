3294702298f715bd3f4c4d60fd8e72d4bcecf1d5
Двойкин Алексей <94dvoikin@gmail.com>
2018-03-07 19:11:45 +0300

    [test] add tests

---
+++ test/PipBenchmark.NetStandard20Test/BenchmarkSuiteTest.cs
@@ -0,0 +1,31 @@
+﻿using PipBenchmark;
+
+namespace PipBenchmark.NetStandard20Test
+{
+    public class BenchmarkSuiteTest : BenchmarkSuite
+    {
+        public BenchmarkSuiteTest()
+            : base("Test", "Measures performance of Test service")
+        {
+            AddBenchmark(CreateBenchmark("Test", "Measures performance of Test", BenchmarkTest));
+        }
+
+        public override void SetUp()
+        {
+            // Some setups
+        }
+
+        public override void TearDown()
+        {
+        }
+
+        public void BenchmarkTest() {
+            var result = 0;
+
+            for (var i = 0; i < 1000; i++) {
+                result += 5;
+                result *= 10;
+            }
+        }
+    }
+}
---
+++ test/PipBenchmark.NetStandard20Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using PipBenchmark;
+using System;
+
+namespace PipBenchmark.NetStandard20Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
