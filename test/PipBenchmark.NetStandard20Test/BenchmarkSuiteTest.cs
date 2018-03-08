abb6cc78cc3d38abed8fc6d7efd8fefe414b194f
Двойкин Алексей <94dvoikin@gmail.com>
2018-03-08 19:23:54 +0300

    [fixes] update proportional execution strategy

--- test/PipBenchmark.NetStandard20Test/BenchmarkSuiteTest.cs
+++ test/PipBenchmark.NetStandard20Test/BenchmarkSuiteTest.cs
@@ -8,6 +8,7 @@ namespace PipBenchmark.NetStandard20Test
             : base("Test", "Measures performance of Test service")
         {
             AddBenchmark(CreateBenchmark("Test", "Measures performance of Test", BenchmarkTest));
+            AddBenchmark(CreateBenchmark("Test2", "Measures performance of Test2", BenchmarkTest2));
         }
 
         public override void SetUp()
@@ -26,6 +27,21 @@ namespace PipBenchmark.NetStandard20Test
                 result += 5;
                 result *= 10;
             }
+
+            System.Console.Out.WriteLine("Execute BenchmarkTest");
+        }
+
+        public void BenchmarkTest2()
+        {
+            var result = 0;
+
+            for (var i = 0; i < 1000; i++)
+            {
+                result += 5;
+                result *= 100;
+            }
+
+            System.Console.Out.WriteLine("Execute BenchmarkTest2");
         }
     }
 }
