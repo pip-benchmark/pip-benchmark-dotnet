dec69ee472448103e6da065e485647812ab8b8ae
artyomgrishchenko <artyomgrishchenko@gmail.com>
2020-01-23 19:46:31 +0200

    Added class RequestDuration to measure elapsed time request

---
+++ src/PipBenchmark.NetStandard20/Runner/Execution/RequestDuration.cs
@@ -0,0 +1,54 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace PipBenchmark.Runner.Execution
+{
+    public class RequestDuration
+    {
+        private class BenchmarkMetric
+        {
+            public readonly string BenchmarkName;
+            public TimeSpan Duration;
+
+            public BenchmarkMetric(string name, TimeSpan duration)
+            {
+                BenchmarkName = name;
+                Duration = duration;
+            }
+        }
+
+        public class BenchmarkGroup
+        {
+            public string BenchmarkName;
+            public double Duration;
+        }
+        
+        private readonly List<BenchmarkMetric> _benchmarkMetrics = new List<BenchmarkMetric> { };
+
+        private readonly Stopwatch _timer = new Stopwatch();
+        
+        public Stopwatch Start()
+        {
+            _timer.Start();
+            return _timer;
+        }
+
+        public void Stop(string benchmarkName)
+        {
+            _timer.Stop();
+            _benchmarkMetrics.Add(new BenchmarkMetric(benchmarkName, _timer.Elapsed));
+        }
+
+        public List<BenchmarkGroup> GetAverageReport()
+        {
+            var report = _benchmarkMetrics
+                .GroupBy(m => m.BenchmarkName)
+                .Select(g => new BenchmarkGroup
+                    {BenchmarkName = g.Key, Duration = g.Average(m => m.Duration.TotalMilliseconds)});
+
+            return report.ToList();
+        }
+    }
+}
