394b02ea2eb25dbc407cbca72b3a76dc4e801106
artyomgrishchenko <artyomgrishchenko@gmail.com>
2020-02-28 22:01:16 +0200

    Added Converter tests

--- Benchmark.Test.NetCore20/ConverterTest.cs
+++ Benchmark.Test.NetCore20/ConverterTest.cs
@@ -9,26 +9,54 @@ namespace Benchmark.Test.NetCore20
         [Fact]
         public void It_Should_Convert_String_To_Long()
         {
-            long x = Converter.StringToLong("123", 0);
-            Assert.Equal(123, x);
+            Assert.Equal(123, Converter.StringToLong("123", 0));
 
-            x = Converter.StringToLong("-123", 0);
-            Assert.Equal(-123, x);
+            Assert.Equal(-123, Converter.StringToLong("-123", 0));
 
-            x = Converter.StringToLong("-123.1545", 0);
-            Assert.Equal(0, x);
+            Assert.Equal(0, Converter.StringToLong("-123.1545", 0));
 
-            // x = Converter.StringToLong("9999999999999999999999999999999999999", 0);
-            // Assert.Equal(0, x);
+            Assert.Equal(9223372036854775807, Converter.StringToLong("9223372036854775807", 0));
 
-            x = Converter.StringToLong("9223372036854775807", 0);
-            Assert.Equal(9223372036854775807, x);
+            Assert.Equal(-9223372036854775808, Converter.StringToLong("-9223372036854775808", 0));
 
-            x = Converter.StringToLong("-9223372036854775808", 0);
-            Assert.Equal(-9223372036854775808, x);
-
-            x = Converter.StringToLong("-922,3372,0368,5477,5808", 0);
-            Assert.Equal(-9223372036854775808, x);
+            Assert.Equal(-9223372036854775808, Converter.StringToLong("-922,3372,0368,5477,5808", 0));
         }
+
+        [Fact]
+        public void It_Should_Convert_Long_To_String()
+        {
+            Assert.Equal("123", Converter.LongToString(123));
+        }
+
+        [Fact]
+        public void It_Should_Double_To_String()
+        {
+            Assert.Equal("123.456", Converter.DoubleToString(123.456));
+        }
+
+        [Fact]
+        public void It_Should_String_To_Double()
+        {
+            Assert.Equal(0, Converter.StringToDouble(null, 0) );
+            Assert.Equal(0, Converter.StringToDouble("ABC", 0));
+            Assert.Equal(123.456, Converter.StringToDouble("123.456", 0));
+        }
+
+        [Fact]
+        public void It_Should_Boolean_To_String()
+        {
+            Assert.Equal("false", Converter.BooleanToString(false));
+            Assert.Equal("true", Converter.BooleanToString(true));
+        }
+        
+        [Fact]
+        public void It_Should_String_To_Boolean()
+        {
+            Assert.False(Converter.StringToBoolean(null, false));
+            Assert.True(Converter.StringToBoolean("True", false));
+            Assert.True(Converter.StringToBoolean("1", false));
+            Assert.True(Converter.StringToBoolean("T", false));
+        }
+
     }
 }
